1892d2c63fd259ebf973ee59b8a217397725f236
Kirtlander <timkosem@gmail.com>
2019-03-14 14:48:21 -0400

    added simple game

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,47 @@
 import './styles.css';
-import { add } from './utils';
+import { add, getRandomInt } from './utils';
 
 console.log('Ready to Party');
-// console.log(add(1, 2));
 
+let tries = 0;
+
+const squares = document.querySelectorAll(".square") as any as HTMLDivElement[];
+const triesEl = document.getElementById("tries");
+const resetBtn = document.querySelector("button");
+let secret: number;
+reset();
+
+function reset() {
+    secret = getRandomInt(0, 5);
+    tries = 0;
+    triesEl.innerText = "";
+    console.log(`the secret is ${secret}`);
+
+    squares.forEach((square, index) => {
+        square.removeEventListener("click", handleClick);
+        square.addEventListener('click', handleClick);
+        square.classList.remove("winner", "loser");
+        if (index === secret) {
+            square.dataset.secret = "true";
+        } else {
+            delete square.dataset.secret;
+        }
+    });
+
+}
+
+function handleClick() {
+    const el = this as HTMLDivElement;
+    el.removeEventListener('click', handleClick);
+    tries++;
+    if (el.dataset.secret === "true") {
+        el.classList.add('winner');
+        triesEl.innerHTML = `You won in ${tries} tries!`
+    } else {
+        el.classList.add('loser');
+    }
+}
+
+resetBtn.addEventListener("click", (e) => {
+    reset();
+});
